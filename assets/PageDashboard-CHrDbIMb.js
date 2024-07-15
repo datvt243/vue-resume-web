b3f0bc2c9a904253f50ed4e855bf03008af8f454
DatVT <datvt@jobtest.vn>
2024-07-15 20:47:34 +0700

    deploy

--- assets/PageDashboard-CHrDbIMb.js
+++
@@ -1,1 +0,0 @@
-import{c as o,a,r,o as t}from"./index-cGN05OM-.js";const s={class:"dashboard"},d={__name:"PageDashboard",setup(c){return(n,_)=>{const e=r("router-view");return t(),o("div",s,[a(e)])}}};export{d as default};
