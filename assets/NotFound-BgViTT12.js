d179aef7fdd94bd45f360a5c14320d07454e3c39
DatVT <datvt@jobtest.vn>
2024-07-16 18:32:35 +0700

    deploy

---
+++ assets/NotFound-BgViTT12.js
@@ -0,0 +1,1 @@
+import{c as t,o as e,b as o}from"./index-BVJ7n5iW.js";const s={class:"auth-container m-auto",style:{"max-width":"500px"}},a=o("p",null,"404!!! Page not Found",-1),n=[a],u={__name:"NotFound",setup(c){return(_,r)=>(e(),t("div",s,n))}};export{u as default};
--- assets/PageDashboard-HViWWqBe.js
+++
@@ -1,1 +0,0 @@
-import{c as o,a,r,o as t}from"./index-DDlzD-Q3.js";const s={class:"dashboard"},d={__name:"PageDashboard",setup(c){return(n,_)=>{const e=r("router-view");return t(),o("div",s,[a(e)])}}};export{d as default};
